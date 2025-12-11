3ca797716cfa43c45351820f2060aac4311c060e
Eugene Mirotin <emirotin@gmail.com>
2025-12-12 00:40:53 +0200

    avoid columns swaps

--- 10/02.ts
+++ 10/02.ts
@@ -38,6 +38,7 @@ const parseDef = (s: string) => {
 };
 
 const zero = new Fraction(0, 1);
+const one = new Fraction(1, 1);
 const isZero = (x: Fraction | undefined) => x !== undefined && x.equals(zero);
 
 const DEBUG = false;
@@ -65,8 +66,6 @@ function diagonalize(matrix: Fraction[][], v: Fraction[]) {
 	matrix = matrix.map((r) => r.slice());
 	v = v.slice();
 
-	const swaps: [number, number][] = [];
-
 	let n = matrix.length;
 	const m = matrix[0]!.length;
 
@@ -90,153 +89,147 @@ function diagonalize(matrix: Fraction[][], v: Fraction[]) {
 		v[r2] = tmpV;
 	};
 
-	const swapCols = (c1: number, c2: number) => {
-		for (let r = 0; r < n; r++) {
-			const v = matrix[r]![c1]!;
-			matrix[r]![c1]! = matrix[r]![c2]!;
-			matrix[r]![c2]! = v;
-		}
-		log(`Swap ${c1} -> ${c2}`);
-		swaps.push([c1, c2]);
-	};
-
-	const sub = (r: number) => {
-		const d = matrix[r]![r]!;
+	const sub = (r: number, c: number) => {
+		const d = matrix[r]![c]!;
 		if (isZero(d)) throw new Error("Zero diagonal element");
 		v[r] = v[r]!.div(d);
-		for (let c = 0; c < m; c++) {
-			matrix[r]![c] = matrix[r]![c]!.div(d);
+		for (let c1 = c; c1 < m; c1++) {
+			matrix[r]![c1] = matrix[r]![c1]!.div(d);
 		}
 
 		for (let r2 = r + 1; r2 < n; r2++) {
-			const d = matrix[r2]![r]!;
+			const d = matrix[r2]![c]!;
 			if (isZero(d)) return;
 			v[r2] = v[r2]!.sub(v[r].mul(d));
-			for (let c = r; c < m; c++) {
-				matrix[r2]![c] = matrix[r2]![c]!.sub(matrix[r]![c]!.mul(d));
+			for (let c1 = c; c1 < m; c1++) {
+				matrix[r2]![c1] = matrix[r2]![c1]!.sub(matrix[r]![c1]!.mul(d));
 			}
 		}
 	};
 
-	for (let r = 0; r < n; r++) {
+	let currCol = 0;
+	for (let r = 0; r < n && r < m; r++) {
 		log(`Row ${r}`);
+
+		// Find the first column that has non-zero values
+		while (
+			currCol < m &&
+			Array.from({ length: n - r }, (_, i) => i + r).every((r2) =>
+				isZero(matrix[r2]![currCol])
+			)
+		) {
+			currCol++;
+		}
+
+		if (currCol >= m) {
+			break;
+		}
+
+		// Move all rows where the next column is not zero up, and the rest down
 		let r2 = r;
 		while (r2 < n) {
-			if (!isZero(matrix[r2]![r]!)) {
+			if (!isZero(matrix[r2]![currCol]!)) {
 				r2++;
 				continue;
 			}
 
 			let found = false;
 			for (let r3 = r2 + 1; r3 < n; r3++) {
-				if (!isZero(matrix[r3]![r]!)) {
-					found = true;
+				if (!isZero(matrix[r3]![currCol]!)) {
 					log("Swap rows");
-					swapRows(r2, r3);
-					print();
-					break;
-				}
-			}
-
-			if (!found) break;
-		}
-
-		if (isZero(matrix[r]![r]!)) {
-			let found = false;
-			for (let c = r + 1; c < m; c++) {
-				if (!isZero(matrix[r]![c]!)) {
 					found = true;
-					log("Swap cols");
-					swapCols(r, c);
+					swapRows(r2, r3);
+					r2++;
 					print();
 					break;
 				}
 			}
+
 			if (!found) break;
 		}
 
-		if (matrix[r]![r] !== undefined && !isZero(matrix[r]![r]!)) {
+		if (!isZero(matrix[r]![currCol]!)) {
 			log("Sub");
-			sub(r);
+			sub(r, currCol);
 			print();
-		}
-
-		for (let r1 = n - 1; r1 > r; r1--) {
-			if (matrix[r1]!.every(isZero)) {
-				if (!isZero(v[r1]!)) throw new Error("Non-zero value in zero row");
-				log(`Removing zero row ${r1}`);
-				matrix.splice(r1, 1);
-				v.splice(r1, 1);
-				n--;
-				print();
-			}
 		}
 	}
 
-	return { matrix, v, swaps };
+	for (let r1 = n - 1; r1 >= 0; r1--) {
+		if (matrix[r1]!.every(isZero)) {
+			if (!isZero(v[r1]!)) throw new Error("Non-zero value in zero row");
+			log(`Removing zero row ${r1}`);
+			matrix.splice(r1, 1);
+			v.splice(r1, 1);
+			n--;
+			print();
+		}
+	}
+
+	return { matrix, v };
 }
 
-function solve(
-	diagonalMatrix: Fraction[][],
-	v: Fraction[],
-	swaps: [number, number][],
-	totalVarsCount: number
-) {
+function solve(diagonalMatrix: Fraction[][], v: Fraction[]) {
+	const totalVarsCount = diagonalMatrix[0]!.length;
 	const freeVarsCount = totalVarsCount - diagonalMatrix.length;
 
-	// coeffs are {freeVarsCount+1} long, first {freeVarsCount} are coefficients for x_free_{i}
+	const freeVarIndices: number[] = [];
+	let currCol = 0;
+	for (let r = 0; r < diagonalMatrix.length; r++) {
+		while (
+			diagonalMatrix[r]![currCol] === undefined ||
+			isZero(diagonalMatrix[r]![currCol])
+		) {
+			freeVarIndices.push(currCol);
+			currCol++;
+		}
+		currCol++;
+	}
+	let nextVar = totalVarsCount - 1;
+	while (freeVarIndices.length < freeVarsCount) {
+		while (freeVarIndices.includes(nextVar)) {
+			nextVar--;
+		}
+		freeVarIndices.push(nextVar);
+		nextVar--;
+	}
+	freeVarIndices.sort((a, b) => a - b);
+
+	log("freeVarIndices: " + freeVarIndices.join(","));
+
+	// coeffs for each var are {freeVarsCount+1} long, first {freeVarsCount} are coefficients for x_free_{i} (left to right),
 	// the last is the free term
 	// so, each x_j = coeffs(-1) + SUM[coeff(i) * freeVar(i)]
-	const coeffs = Array.from({ length: freeVarsCount }, (_, i) =>
-		Array.from({ length: freeVarsCount + 1 }, (_, j) =>
-			i === j ? new Fraction(1, 1) : zero
-		)
-	);
+	// which is also true if x_j is a free var itself
+	const coeffs = Array.from({ length: totalVarsCount }, () => [] as Fraction[]);
 
-	for (let i = totalVarsCount - freeVarsCount - 1; i >= 0; i--) {
-		const row = diagonalMatrix[i]!;
+	let currRow = diagonalMatrix.length;
+	for (let i = totalVarsCount - 1; i >= 0; i--) {
+		const k = freeVarIndices.indexOf(i);
+		if (k >= 0) {
+			coeffs[i] = Array.from({ length: freeVarsCount + 1 }, (_, j) =>
+				j === k ? one : zero
+			);
+			continue;
+		}
+
+		currRow--;
+		const row = diagonalMatrix[currRow]!;
 		const currCoeffs = Array.from({ length: freeVarsCount + 1 }, () => zero);
-		currCoeffs[currCoeffs.length - 1] = v[i]!;
+		currCoeffs[currCoeffs.length - 1] = v[currRow]!;
 		for (let j = totalVarsCount - 1; j > i; j--) {
 			const c = row[j]!;
-			const jCoeffs = coeffs[j - i - 1]!;
+			const jCoeffs = coeffs[j]!;
 			for (let p = freeVarsCount; p >= 0; p--) {
 				currCoeffs[p] = currCoeffs[p]!.sub(jCoeffs[p]!.mul(c));
 			}
 		}
-
-		coeffs.unshift(currCoeffs);
+		coeffs[i] = currCoeffs;
 	}
-
-	// trace all variables to the original button numbers
-	const originalVarIndices = Array.from(
-		{ length: totalVarsCount },
-		(_, i) => i
-	);
-	for (let i = totalVarsCount - 1; i >= 0; i--) {
-		let b = i;
-		for (const [from, to] of swaps.toReversed()) {
-			if (b === to) {
-				b = from;
-			} else if (b === from) {
-				b = to;
-			}
-		}
-		originalVarIndices[i] = b;
-	}
-	const freeVarIndices = Array.from(
-		{ length: freeVarsCount },
-		(_, i) => originalVarIndices[totalVarsCount - freeVarsCount + i]
-	);
 
 	log("Free vars: " + freeVarIndices.join(", "));
 
-	const originalCoeffs: Fraction[][] = [];
-	for (const [newIndex, originalIndex] of originalVarIndices.entries()) {
-		originalCoeffs[originalIndex] = coeffs[newIndex]!;
-	}
-
-	return { freeVarIndices, coeffs: originalCoeffs };
+	return { freeVarIndices, coeffs };
 }
 
 function findOptimal(targetValues: number[], adjMatrix: number[][]): number {
@@ -244,21 +237,16 @@ function findOptimal(targetValues: number[], adjMatrix: number[][]): number {
 	const m = adjMatrix.length;
 	const originaMatrix = Array.from({ length: n }, (_, r) =>
 		Array.from({ length: m }, (_, c) =>
-			adjMatrix[c]?.includes(r) ? new Fraction(1, 1) : new Fraction(0, 1)
+			adjMatrix[c]?.includes(r) ? one : zero
 		)
 	);
 
-	const { matrix, v, swaps } = diagonalize(
+	const { matrix, v } = diagonalize(
 		originaMatrix,
 		targetValues.map((v) => new Fraction(v, 1))
 	);
 
-	const { freeVarIndices: freeButtonIndices, coeffs } = solve(
-		matrix,
-		v,
-		swaps,
-		m
-	);
+	const { freeVarIndices: freeButtonIndices, coeffs } = solve(matrix, v);
 
 	let bestResult = Infinity;
 	let bestPresses: Fraction[] = [];
@@ -318,7 +306,7 @@ function findOptimal(targetValues: number[], adjMatrix: number[][]): number {
 }
 
 // .filter(
-// 	(d) => d.jolts.join(",") === "26,36,33,26,32,11"
+// 	(d) => d.jolts.join(",") === "65,97,74,63,32,50,72,115"
 // )
 
 async function main() {
