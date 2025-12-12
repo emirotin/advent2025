09cacda7c921caeeb83a8a599cbae7f8c7486bfe
Eugene Mirotin <emirotin@gmail.com>
2025-12-12 15:57:08 +0200

    working solution, too slow

--- 12/01.ts
+++ 12/01.ts
@@ -8,11 +8,23 @@ async function readInput() {
 type Matrix = {
 	w: number;
 	h: number;
-	m: (1 | 0)[][];
+	m: boolean[][];
+	emptyCells: Array<readonly [number, number]>;
 };
 
-const initMatrix = (w: number, h: number) =>
-	Array.from({ length: h }, () => Array.from({ length: w }, () => 0 as 0 | 1));
+const initMatrix = (w: number, h: number): Matrix => {
+	const m = Array.from({ length: h }, () =>
+		Array.from({ length: w }, () => false)
+	);
+	return {
+		w,
+		h,
+		m,
+		emptyCells: Array.from({ length: h }, (_, r) =>
+			Array.from({ length: w }, (_, c) => [r, c] as const)
+		).flat(),
+	};
+};
 
 const parseShape = (s: string, i: number): Matrix => {
 	const lines = s.trim().split("\n");
@@ -22,12 +34,19 @@ const parseShape = (s: string, i: number): Matrix => {
 		l
 			.trim()
 			.split("")
-			.map((s) => (s === "#" ? 1 : 0))
+			.map((s) => s === "#")
 	);
+	const h = m.length;
+	const w = m[0]!.length;
 	return {
 		m,
-		h: m.length,
-		w: m[0]!.length,
+		h,
+		w,
+		emptyCells: Array.from({ length: h }, (_, r) =>
+			Array.from({ length: w }, (_, c) => [r, c] as const)
+		)
+			.flat()
+			.filter(([r, c]) => !m[r]![c]),
 	};
 };
 
@@ -47,20 +66,18 @@ const parseProblems = (s: string) => {
 		});
 };
 
+type Problem = ReturnType<typeof parseProblems>[number];
+
 const rotateCCW = (matrix: Matrix): Matrix => {
 	const { w, h, m } = matrix;
 	const newM = initMatrix(h, w);
 	for (let r = 0; r < h; r++) {
 		for (let c = 0; c < w; c++) {
-			newM[w - c - 1]![r] = m[r]![c]!;
+			newM.m[w - c - 1]![r] = m[r]![c]!;
 		}
 	}
 
-	return {
-		w: matrix.h,
-		h: matrix.w,
-		m: newM,
-	};
+	return newM;
 };
 
 const flipH = (matrix: Matrix): Matrix => {
@@ -68,15 +85,11 @@ const flipH = (matrix: Matrix): Matrix => {
 	const newM = initMatrix(w, h);
 	for (let r = 0; r < h; r++) {
 		for (let c = 0; c < w; c++) {
-			newM[r]![w - c - 1] = m[r]![c]!;
+			newM.m[r]![w - c - 1] = m[r]![c]!;
 		}
 	}
 
-	return {
-		w: matrix.h,
-		h: matrix.w,
-		m: newM,
-	};
+	return newM;
 };
 
 const flipV = (matrix: Matrix): Matrix => {
@@ -84,15 +97,11 @@ const flipV = (matrix: Matrix): Matrix => {
 	const newM = initMatrix(w, h);
 	for (let r = 0; r < h; r++) {
 		for (let c = 0; c < w; c++) {
-			newM[h - r - 1]![c] = m[r]![c]!;
+			newM.m[h - r - 1]![c] = m[r]![c]!;
 		}
 	}
 
-	return {
-		w: matrix.h,
-		h: matrix.w,
-		m: newM,
-	};
+	return newM;
 };
 
 const clone = (matrix: Matrix): Matrix => {
@@ -100,15 +109,11 @@ const clone = (matrix: Matrix): Matrix => {
 	const newM = initMatrix(w, h);
 	for (let r = 0; r < h; r++) {
 		for (let c = 0; c < w; c++) {
-			newM[r]![c] = m[r]![c]!;
+			newM.m[r]![c] = m[r]![c]!;
 		}
 	}
 
-	return {
-		w: matrix.h,
-		h: matrix.w,
-		m: newM,
-	};
+	return { ...newM, emptyCells: matrix.emptyCells.slice() };
 };
 
 const eq = (m1: Matrix, m2: Matrix) => {
@@ -125,7 +130,7 @@ const eq = (m1: Matrix, m2: Matrix) => {
 
 const print = (matrix: Matrix) => {
 	console.log(
-		matrix.m.map((r) => r.map((x) => (x === 1 ? "#" : ".")).join("")).join("\n")
+		matrix.m.map((r) => r.map((x) => (x ? "#" : ".")).join("")).join("\n")
 	);
 };
 
@@ -147,9 +152,96 @@ const produceVariations = (matrix: Matrix) => {
 	const m2 = rotateCCW(m1);
 	const m3 = rotateCCW(m2);
 	const result = [matrix, m1, m2, m3, flipH(matrix), flipV(matrix)];
-	result.forEach(print);
 	removeDuplicates(result, eq);
 	return result;
+};
+
+const fitShape = (
+	host: Matrix,
+	shape: Matrix,
+	startR: number,
+	startC: number
+): Matrix | null => {
+	const newHost = clone(host);
+	const emptyCells = new Set(host.emptyCells.map(([r, c]) => `${r}:${c}`));
+
+	for (let r = 0; r < shape.h; r++) {
+		const hostR = startR + r;
+		if (hostR >= host.h) return null;
+		for (let c = 0; c < shape.w; c++) {
+			const hostC = startC + c;
+			if (hostC >= host.w) return null;
+			if (shape.m[r]![c]!) {
+				const coords = `${hostR}:${hostC}`;
+				if (!emptyCells.has(coords)) return null;
+				emptyCells.delete(coords);
+				newHost.m[hostR]![hostC] = shape.m[r]![c]!;
+			}
+		}
+	}
+
+	return {
+		...newHost,
+		emptyCells: emptyCells
+			.values()
+			.toArray()
+			.map(
+				(s) =>
+					s.split(":").map((x) => Number.parseInt(x)) as unknown as readonly [
+						number,
+						number
+					]
+			),
+	};
+};
+
+const fitIfPossibleInt = (
+	p: Problem,
+	shapes: Matrix[][],
+	currentState: Matrix
+): Matrix | null => {
+	if (p.w !== currentState.w || p.h !== currentState.h)
+		throw new Error("Wrong state dimensions");
+	if (p.shapeCounts.length !== shapes.length)
+		throw new Error("Wrong shapes count");
+
+	const i = p.shapeCounts.findIndex((x) => x > 0);
+	// all zeros, good
+	if (i < 0) return currentState;
+
+	const shape = shapes[i]!;
+	const newCounts = p.shapeCounts.slice();
+	newCounts[i]! -= 1;
+	const newP = {
+		...p,
+		shapeCounts: newCounts,
+	};
+
+	for (const m of shape) {
+		for (const [r, c] of currentState.emptyCells) {
+			const newState = fitShape(currentState, m, r, c);
+			if (!newState) continue;
+			const res = fitIfPossibleInt(newP, shapes, newState);
+			if (res) return res;
+		}
+	}
+
+	return null;
+};
+
+const fitIfPossible = (p: Problem, shapes: Matrix[][]) => {
+	const totalArea = p.w * p.h;
+	const shapeAreas = shapes.map((shape) => {
+		const v = shape[0]!;
+		return v.m
+			.map((row) => row.reduce<number>((a, b) => a + +b, 0))
+			.reduce((a, b) => a + b);
+	});
+	const shapesTotalArea = shapeAreas
+		.map((a, i) => a * p.shapeCounts[i]!)
+		.reduce((a, b) => a + b);
+	if (shapesTotalArea > totalArea) return null;
+	return fitIfPossibleInt(p, shapes, initMatrix(p.w, p.h));
 };
 
 async function main() {
@@ -157,6 +249,16 @@ async function main() {
 
 	const problems = parseProblems(input.pop()!);
 	const shapes = input.map(parseShape).map(produceVariations);
+
+	for (const p of problems) {
+		console.log("----------");
+		const r = fitIfPossible(p, shapes);
+		if (r) {
+			print(r);
+		} else {
+			console.log("No Way!");
+		}
+	}
 }
 
 main().catch(console.error);
